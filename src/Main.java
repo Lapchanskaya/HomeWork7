403139ae978e0b55824b00c2983adfa4b5b62e27
Svetlana <isaeva322@mail.ru>
2022-11-26 18:53:15 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+        System.out.println( " Задание 1");
+
+     int goal = 2_459_000;
+     int deposit = 15_000;
+     int month = 0;
+     int saving = 0;
+
+     while ( saving <= goal) {
+         saving+=saving/100;
+         saving+=deposit;
+         month++;
+         System.out.println( " Месяц " + month + " , сумма накоплений равна " + saving + " рублей.");
+     }
+
+
+
     }
 }
